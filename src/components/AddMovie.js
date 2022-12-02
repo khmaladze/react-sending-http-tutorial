f78cd26326c491b22f8051ea4a27d3853398d779
khmaladze <khmaladze.g.7@gmail.com>
2022-12-02 05:24:04 +0400

    first commit

---
+++ src/components/AddMovie.js
@@ -0,0 +1,49 @@
+import React, { useRef } from "react";
+import classes from "./AddMovie.module.css";
+
+function AddMovie(props) {
+  const titleRef = useRef("");
+  const openingTextRef = useRef("");
+  const releaseDateRef = useRef("");
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const movie = {
+      title: titleRef.current.value,
+      openingText: openingTextRef.current.value,
+      releaseDate: releaseDateRef.current.value,
+    };
+
+    if (
+      movie.title.length <= 0 ||
+      movie.openingText.length <= 0 ||
+      movie.releaseDate.length <= 0
+    ) {
+      alert("add all fields");
+      return;
+    }
+
+    props.onAddMovie(movie);
+  }
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className={classes.control}>
+        <label htmlFor="title">Title</label>
+        <input type="text" id="title" ref={titleRef} />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="opening-text">Opening Text</label>
+        <textarea rows="5" id="opening-text" ref={openingTextRef}></textarea>
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="date">Release Date</label>
+        <input type="text" id="date" ref={releaseDateRef} />
+      </div>
+      <button>Add Movie</button>
+    </form>
+  );
+}
+
+export default AddMovie;
